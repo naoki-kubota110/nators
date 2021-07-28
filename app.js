64ece7863aa7e6825a7c877e7fff3c137d1a3dd0
naoki-kubota110 <naoki.kubota@rakus-partners.co.jp>
2021-07-28 11:30:46 +0900

    リファクタリング

--- app.js
+++ app.js
@@ -3,23 +3,29 @@ const app  = express();
 const fs = require("fs")
 app.use(express.json())
 
-// app.get('/', (req, res) => {
-//   res.send("hello from the server side")
-// })
-// app.post("/",(req, res) => {
-//   res.send("yo can post ")
-// })
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
-app.get("/api/v1/tours", (req,res) => {
-  res.status(200).json({status: "success", results: tours.length, data: {tours}})
-})
-app.get("/api/v1/tours/:id", (req,res) => {
-  console.log(req.params)
-  res.status(200).json({status: "success"})
-})
-app.post("/api/v1/tours", (req,res) => {
-  // console.log(req.body)
+const getAllTours = (req,res) => {
+    res.status(200).json({
+      status: "success",
+      result: tours.length,
+      data: {
+        tours
+      }
+    })
+  }
+const getTour =  (req,res) => {
+  const id = req.params.id* 1
+  if(id > tours.length){
+    return res.status(404).json({
+      status: "fail",
+      message: "Invaild ID"
+    })
+  }
+  const tour = tours.find(el => el.id === id)
+  res.status(200).json({status: "success", results: tours.length, data: {tour}})
+}
+const createTour = (req,res) => {
   const newId = tours[tours.length -1].id + 1
   const newTour = Object.assign({id: newId}, req.body)
   tours.push(newTour)
@@ -32,9 +38,49 @@ app.post("/api/v1/tours", (req,res) => {
       }
     })
   })
-  // res.send("Done")
-})
-const port = 3000
+  const updateTour = (req,res) => {
+    if(req.params.id * 1 > tours.length) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Invaild ID"
+      })
+    }
+    res.status(200).json({
+      status: "success",
+      data: {tour: "update Tour!"}
+    })
+  }
+
+  const deleteTour = (req,res) => {
+    if(req.params.id * 1 > tours.length) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Invaild ID"
+      })
+    }
+    res.status(204).json({
+      status: "success",
+      data: null
+    })
+  }
+
+// app.get("/api/v1/tours", getAllTours)
+// app.get("/api/v1/tours/:id", getTour)
+// app.post("/api/v1/tours", createTour)
+// app.patch("/api/v1/tours/:id", updateTour)
+// app.delete("/api/v1/tours/:id", deleteTour)
+
+app
+  .route("/api/v1/tours")
+  .get(getAllTours)
+  .post(createTour)
+app
+  .route("/api/v1/tours/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour)
+
+const port = 3000;
 app.listen(port, () => {
   console.log("App runnig on port")
 })
