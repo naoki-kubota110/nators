d5a64c8d360fb43dc762de72ea366f52bfbea966
naoki-kubota110 <naoki.kubota@rakus-partners.co.jp>
2021-08-04 17:44:23 +0900

    filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,25 @@ const Tour = require("./../models/tourModel")
 
 exports.getAllTours = async (req,res) => {
   try{
+    // console.log(req.query)
+    //1) Filtering
+    const queryObj = {...req.query}
+    const excludeedFields = ["page", "sort", "limit", "fields"]
+    excludeedFields.forEach(el => delete  queryObj[el])
 
-    const tours = await Tour.find()
+    //2) Advansed Filtering
+    let queryStr = JSON.stringify(queryObj)
+    queryStr  = queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match => `$${match}`)
+    console.log(JSON.parse(queryStr))
+    let query = Tour.find(JSON.parse(queryStr))
+    //3) sorting
+    if(req.query.sort){
+      query = query.sort(req.query.sort)
+    }
+    const tours = await query
+
+
+    // const tours = await Tour.find().where("duration").equals(5).where("difficulty").equals("easy")
     console.log(req.requestTime)
     res.status(200).json({
       status: "success",
--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -4,9 +4,10 @@ const dotenv = require("dotenv")
 const Tour = require("./../../models/tourModel")
 
 dotenv.config({path: "./config.env"})
-const app = require("./app")
+
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD)
 mongoose.connect(DB, {
+  useUnifiedTopology : true,
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false
@@ -36,4 +37,9 @@ const deleteData = async () => {
   }
   catch(err){
   }
+}
+if(process.argv[2] === "--import"){
+  importData()
+}else if(process.argv[2] === "--delete"){
+  deleteData()
 }
